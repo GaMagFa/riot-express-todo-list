dcaf7e3e3e40647a62b621252cc730c6c8b24590
Gabriele Fahrer <gabrielemf0325@gmail.com>
2020-09-11 18:53:32 +0200

    corrected errors in deploy.js

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '54.159.244.13',
       username: 'ubuntu',
-      privateKey: 'hs-key.pem'
+      privateKey: 'rtd-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
